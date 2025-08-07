09945d9facce05a80ef26f2d0c1e03ccc5cd70ee
Bhanu Tripathi <26444713+bhanu75@users.noreply.github.com>
2025-08-08 00:29:40 +0530

    Create UserList.js

---
+++ src/components/UserList.js
@@ -0,0 +1,31 @@
+// src/components/UserList.js
+import React, { useEffect, useState } from 'react';
+
+const UserList = () => {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users') // Free public API
+      .then(response => response.json())
+      .then(data => {
+        setUsers(data);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) return <p>Loading users...</p>;
+
+  return (
+    <div>
+      <h2>User List</h2>
+      <ul>
+        {users.map(user => (
+          <li key={user.id}>{user.name} - 📧 {user.email}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default UserList;
