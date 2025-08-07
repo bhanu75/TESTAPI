90a1059ad37bd625ad74b044337a0f352a3f6871
Bhanu Tripathi <26444713+bhanu75@users.noreply.github.com>
2025-08-08 00:30:16 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// src/App.js
+import React from 'react';
+import UserList from './components/UserList';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>API Call Example: Users</h1>
+      <UserList />
+    </div>
+  );
+}
+
+export default App;
